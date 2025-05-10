e9bc7590bbc404c548789f335df2f7dea780f099
Johnson-69 <johnsonsoenyunt@gmail.com>
2025-05-10 17:02:56 +0630

    Created: cms_controller_namespaces

---
+++ websites/cms_controller_namespaces/Ijdb/Controllers/Register.php
@@ -0,0 +1,11 @@
+<?php
+namespace Ijdb\Controllers;
+class Register {
+	public function __construct(private \Ninja\DatabaseTable $authorsTable) {
+
+	}
+
+	public function register() {
+		//sample function for a second controller
+	}
+}
